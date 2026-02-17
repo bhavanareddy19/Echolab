4bd2856cd3bd7f92b50237cb58e83a3e044e3912
Bhavana Reddy <96643719+bhavanareddy19@users.noreply.github.com>
2026-02-16 23:24:13 -0700

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${mavenPro.variable} ${inter.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${mavenPro.variable} ${inter.variable}`} suppressHydrationWarning>
         {children}
       </body>
     </html>
